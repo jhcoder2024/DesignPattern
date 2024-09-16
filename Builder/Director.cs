b47770a5e82acb6a34e354c384101551ef7046f7
jhernandez2019 <jorge.hernandez@ex-cle.com>
2024-09-15 21:38:34 -0400

    Build Pattern

---
+++ Builder/Director.cs
@@ -0,0 +1,23 @@
+﻿namespace DesignPattern.Builder;
+
+public class Director
+{
+    private IBuilder _builder;
+
+    public IBuilder Builder
+    {
+        set => _builder = value;
+    }
+
+    public void BuildMinimalViableProduct()
+    {
+        _builder.BuildPartA();
+    }
+
+    public void BuildFullFeatureProduct()
+    {
+        _builder.BuildPartA();
+        _builder.BuildPartB();
+        _builder.BuildPartC();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DesignPattern.Builder;
+
+namespace DesignPattern;
+
+class Program{
+    public static void Main()
+    {
+        //new ClientAbstractFactory().Main(); //AbstractFactoryExample
+        new ClientBuild().Main(); //AbstractFactoryExample
+    }
+}
